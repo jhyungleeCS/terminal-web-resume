eb8b19af1b697f7d429979da94ef89129bf310d3
Daniel Lee <jhyunglee.cs@gmail.com>
2022-09-06 16:27:53 -0400

    first attempt.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # terminal-web-resume
 
 Hello Everyone! My name is Joo Hyung Lee, or Daniel Lee. I usually go by mangoclient on most of my socials. 
+
+This is my first web application project that I wanted to work on to help kickstart my javascript as well as touching up on my md langauges. 
+
+A particular way that I wanted to display my resume was using a website to help showcase my projects, blogs, and research through a command-line/TERMINAL like style website.
+
+I got my inspiration off of: https://fkcodes.com/ 
--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-let div = document.getElementById("first-line");
-div.insertAdjacentText('beforebegin', 'Daniel Lee@mangoclient [~] $: ');
 
-let command = document.getElementById("command-el");
-command.addEventListener('keypress', function(){
-    //display a paragraph
-})
+//help array command displaying help commands
+const help = [
+    'whoami             Who is mangoclient?',
+    'projects           View projects',
+    'contacts           View contact information',
+    'resume             Link to LaTeX resume',
+    'help               View commands'
+]
+
+//whoami array
+//projects array
+//contacts array
+//resume array
+
+//Function to create before div text
+let textElement = document.getElementById("text-element");
+//Wasn't sure how to use the pseudo ::before element here. Fix later. 
+textElement.insertAdjacentText('beforebegin', 'Daniel Lee@mangoclient [~] $: ');
+
+let textResult = document.getElementById("text-result");
+let textHere = document.getElementById("text-here");
+
+//Start of Function to display input text into <p id="text-here">
+//use for loop or if else statement? 
+let inputElement = document.getElementById("input-element");
+let listElement = document.getElementById("myList");
+
+function generateList(){
+    help.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        listElement.appendChild(li);
+    })
+}
+
+if (inputElement.value.toLowerCase() === 'help') {
+    generateList()
+}else {
+    textHere.innerHTML="Didn't enter help"
+}
+
+
 
