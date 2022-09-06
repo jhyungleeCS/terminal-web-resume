4486d2202af6450ba29dbec2ceee604af56b4ef0
Daniel Lee <jhyunglee.cs@gmail.com>
2022-09-06 03:50:58 -0400

    first edit

---
+++ index.js
@@ -0,0 +1,8 @@
+let div = document.getElementById("first-line");
+div.insertAdjacentText('beforebegin', 'Daniel Lee@mangoclient [~] $: ');
+
+let command = document.getElementById("command-el");
+command.addEventListener('keypress', function(){
+    //display a paragraph
+})
+
